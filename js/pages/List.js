3c8975dc433af0b3b68162b111ac0695da69ca9a
nike342 <93024973+nike342@users.noreply.github.com>
2025-10-25 22:46:22 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -118,7 +118,7 @@ export default {
                         Do not use secret routes or bug routes
                     </p>
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        The recording must include an easily visible cheat indicator.
                     </p>
                     <p>
                         Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
